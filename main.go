276d81dcf9e54edc4852fbedde5ebba7c42e15ff
Inoue Shota <inoue@innova-jp.com>
2015-07-24 19:08:26 +0900

    Qiita検索機能をシンプルに実装

--- main.go
+++ main.go
@@ -2,33 +2,17 @@ package main
 
 import (
     "fmt"
-    "net/http"
     "os"
 
-    "github.com/PuerkitoBio/goquery"
+    "github.com/skratchdot/open-golang/open"
 )
 
 func main() {
     if len(os.Args) != 2 {
-        fmt.Fprintln(os.Stderr, "yomikata [word]")
+        fmt.Fprintln(os.Stderr, "qiita-go [word]")
         os.Exit(1)
     }
     word := os.Args[1]
-    resp, err := http.Get("http://yomikata.org/word/" + word)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, os.Args[0]+":", err)
-        os.Exit(1)
-    }
-    doc, err := goquery.NewDocumentFromResponse(resp)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, os.Args[0]+":", err)
-        os.Exit(1)
-    }
-    if doc.Find("#word").Text() == "" {
-        fmt.Fprintln(os.Stderr, "わかりません")
-        os.Exit(1)
-    }
-    doc.Find(".spAns").Each(func(_ int, s *goquery.Selection) {
-        fmt.Printf("%s (%s)\n", s.Find(".psAns").Text(), s.Find(".psPt").Text())
-    })
+    url := "http://qiita.com/search?utf8=✓&sort=rel&q=" + word
+    open.Run(url)
 }
